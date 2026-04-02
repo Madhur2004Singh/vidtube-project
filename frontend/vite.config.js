66c3343a85a5b541315342b9d461bdb7bf3968a3
Jayshu <madhursingh2704@gmail.com>
2026-04-03 00:41:19 +0530

    Changed the backend alias to an env variable VITE_BACKEND_URL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
@@ -8,7 +8,8 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:8000", // your backend port
+        // target: "http://localhost:8000", // your backend port
+        target: env.VITE_BACKEND_URL,
         changeOrigin: true,
       },
     },
